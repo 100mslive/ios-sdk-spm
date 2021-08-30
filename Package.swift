e038ab7e0130cdaa15b3490c9018eb377e4be1b6
Dmitry Fedoseyev <dfedoseyev@gmail.com>
2021-08-30 17:29:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "HMSSDK",
-            targets: ["HMSSDK"]),
+            targets: ["HMSSDK", "WebRTC"]),
     ],
     dependencies: [],
     targets: [
